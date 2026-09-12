6dbc426fbefdb24c55b295d3baef7290696d9ba7
Uppalapati Shanmukh Srinivas <uppalapatishanmukh2026@gmail.com>
2026-09-12 18:28:51 +0530

    Fix CORS for frontend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from openai import OpenAI
